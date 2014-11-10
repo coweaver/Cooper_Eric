4dd3eb865ae62f717a7bfeae24a3323945392474
coweaver <cooperweaver@mac.com>
2014-11-10 14:06:55 -0500

    hi

--- getpage.py
+++ getpage.py
@@ -2,6 +2,7 @@ import urllib, urllib2, httplib, base64
 import json
 
 
+
 headers = { 
     # Basic Authorization Sample 
     # 'Authorization': 'Basic %s' % base64.encodestring('{username}:{password}'), 
@@ -9,20 +10,29 @@ headers = {
 
 params = urllib.urlencode({'key': 'BB75E8A7-F767-47B0-B17F-BD16E94E3D8B'}) 
 
-try:
-        conn = httplib.HTTPConnection('api.nfldata.apiphany.com')
-        # Specify values for path parameters (shown as {...}) and request body if needed 
+conn = httplib.HTTPConnection('api.nfldata.apiphany.com') 
 
-        team = "NYJ"
-        conn.request("GET", "/trial/json/Players/"+team+"?%s" % params, "", headers)
-        response = conn.getresponse()
-        conn.request("GET", "/trial/json/Player/732?%s" % params, "", headers) 
-       
-        data = response.read()
+team = "NYJ"
+conn.request("GET", "/trial/json/Players/"+team+"?%s" % params, "", headers) 
+
+response = conn.getresponse() 
+data = response.read() 
+
+d = json.loads(data)
+
+
+
+
+
+
+for player in d:
+        print player["Name"]
+        try:
+                print player["PlayerSeason"]["FantasyPoints"]
+        except:
+                print 0
+conn.close() 
+
+
         
-        print(data) 
-        conn.close()
-except ValueError:
-        print "Oops! didn't work"
-
-
+        
